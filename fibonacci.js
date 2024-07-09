f401ece60a5101db9f336a119fb0e1e1ab58bcd5
arta <artafarshadi1996gmail.com>
2024-07-08 21:04:58 -0700

    Add correct Fibonacci sequence code

--- fibonacci.js
+++ fibonacci.js
@@ -1,2 +1,17 @@
-C:\Users\Arta F\Downloads\Febonacci.js to fibonacci.js
+// Function to calculate the Fibonacci sequence
+function fibonacci(n) {
+    if (n === 0) {
+        return 0;
+    }
+    if (n === 1) {
+        return 1;
+    }
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+// Print Fibonacci sequence up to n terms
+const numberOfTerms = 10; // Feel free to change this to get more or fewer terms
+for (let i = 0; i < numberOfTerms; i++) {
+    console.log(fibonacci(i));
+}
 
